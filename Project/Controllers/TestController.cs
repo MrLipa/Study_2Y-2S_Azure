9c99e83492cb706c902954023191c26cffe39e4c
MrLipa <tomek.szkaradek14@gmail.com>
2024-01-08 19:29:23 +0100

    init

---
+++ Project/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("To jest testowa wiadomość z kontrolera TestController.");
+        }
+    }
+}
